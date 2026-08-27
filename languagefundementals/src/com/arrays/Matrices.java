0c4e4eba2e744bd9649fe1b9c2e8e22db5651014
venu2213 <eragalavenugopal@gmail.com>
2026-08-27 12:08:30 +0530

    matrices

---
+++ languagefundementals/src/com/arrays/Matrices.java
@@ -0,0 +1,51 @@
+package com.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matrices {
+
+	public static void main(String[] args) {
+		 System.out.println("main method started");
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter row size:");
+		 int n=sc.nextInt();
+		 System.out.println("Enter column size:");
+		 int n1=sc.nextInt();
+		 int[][] arr=new int[3][3];
+		 for(int i=0;i<arr.length;i++) {
+			 System.out.println("Enter the Elements:");
+			 for(int j=0;j<arr.length;j++) {
+				 arr[i][j]=sc.nextInt();
+			 }
+		 }
+		 System.out.println(Arrays.deepToString(arr));
+		 for(int i=0;i<arr.length;i++) {
+			 for(int j=0;j<arr.length;j++) {
+				 int start=0;
+				  int end=n-1;
+				  while(start<end) {
+					  int temp=arr[i][start];
+					  arr[i][start]=arr[i][end];
+					  arr[i][end]=temp;
+					  start++;
+					  end--;
+				  }
+			 }
+		 }
+		 for(int i=0;i<arr.length;i++) {
+			 for(int j=0;j<arr.length;j++) {
+				if(i!=n/2||j!=n/2) { 
+					arr[i][j]=arr[i][j]*2;
+				}
+			 }
+		 }
+		 for(int i=0;i<arr.length;i++) {
+			 arr[i][i]=arr[i][i]*arr[i][i];
+		 }
+		 System.out.println(Arrays.deepToString(arr));
+		 System.out.println("main method ended");
+
+	}
+
+}
