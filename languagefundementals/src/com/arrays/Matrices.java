ea16d106a3dc957ab1c513708fa73eba39c7de34
venu2213 <eragalavenugopal@gmail.com>
2026-08-29 19:27:36 +0530

    arrays

--- languagefundementals/src/com/arrays/Matrices.java
+++ languagefundementals/src/com/arrays/Matrices.java
@@ -6,45 +6,56 @@ import java.util.Scanner;
 public class Matrices {
 
 	public static void main(String[] args) {
-		 System.out.println("main method started");
-		 Scanner sc=new Scanner(System.in);
-		 System.out.println("Enter row size:");
-		 int n=sc.nextInt();
-		 System.out.println("Enter column size:");
-		 int n1=sc.nextInt();
-		 int[][] arr=new int[3][3];
-		 for(int i=0;i<arr.length;i++) {
-			 System.out.println("Enter the Elements:");
-			 for(int j=0;j<arr.length;j++) {
-				 arr[i][j]=sc.nextInt();
-			 }
-		 }
-		 System.out.println(Arrays.deepToString(arr));
-		 for(int i=0;i<arr.length;i++) {
-			 for(int j=0;j<arr.length;j++) {
-				 int start=0;
-				  int end=n-1;
-				  while(start<end) {
-					  int temp=arr[i][start];
-					  arr[i][start]=arr[i][end];
-					  arr[i][end]=temp;
-					  start++;
-					  end--;
-				  }
-				  if(i==n/2||j==n/2) { 
-						arr[i][j]=arr[i][j]*2;
+		System.out.println("main method started");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter row size:");
+		int n = sc.nextInt();
+		System.out.println("Enter column size:");
+		int n1 = sc.nextInt();
+		int[][] arr = new int[3][3];
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Enter the Elements:");
+			for (int j = 0; j < arr.length; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println(Arrays.deepToString(arr));
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i; j++) {
+				int start = 0;
+				int end = n - 1;
+				while (start < end) {
+					int temp = arr[i][start];
+					arr[i][start] = arr[i][end];
+					arr[i][end] = temp;
+					start++;
+					end--;
+				}
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 1) {
+				for (int j = 0; j < arr.length; j++) {
+					if (j != n / 2) {
+						arr[i][j] = arr[i][j] * 2;
 					}
-			 }
-		 }
-		 for(int i=0;i<arr.length;i++) {
-			 for(int j=0;j<arr.length;j++) {
-				 if(i==j) {
-					 arr[i][j]=arr[i][j]*arr[i][j];
-				 }
-			 }
-		 }
-		 System.out.println(Arrays.deepToString(arr));
-		 System.out.println("main method ended");
+				}
+
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (i == j) {
+					arr[i][j] = arr[i][j] * arr[i][j];
+				}
+			}
+		}
+		
+		System.out.println(Arrays.deepToString(arr));
+		System.out.println("main method ended");
 
 	}
 
--- languagefundementals/src/com/arrays/TransposeMatrices.java
+++ languagefundementals/src/com/arrays/TransposeMatrices.java
@@ -1,38 +1,34 @@
 package com.arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TransposeMatrices {
 
 	public static void main(String[] args) {
 		System.out.println("main method started");
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter size of row:");
-		int n=sc.nextInt();
+		int n = sc.nextInt();
 		System.out.println("Enter size of column:");
-		int n1=sc.nextInt();
-		int[][] arr=new int[n][n1];
-		System.out.println("Enter the Elements:");
-		for(int i=0;i<arr.length;i++) {
+		int n1 = sc.nextInt();
+		int[][] arr = new int[n][n1];
+		int[][] transpose = new int[n][n1];
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println("Enter the Elements:");
-			for(int j=0;j<arr.length;j++) {
-				arr[i][j]=sc.nextInt();
+			for (int j = 0; j < arr.length; j++) {
+				arr[i][j] = sc.nextInt();
 			}
 		}
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr.length;j++) {
-				System.out.println(arr[i][j] +" ");
+		System.out.println(Arrays.deepToString(arr));
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				transpose[j][i] = arr[i][j];
 			}
-			System.out.println( );
 		}
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr.length;j++) {
-				System.out.println(arr[j][i] +" ");
-			}
-			System.out.println( );
-		}
+		System.out.println(Arrays.deepToString(transpose));
+
 		System.out.println("main method ended");
-		 
 
 	}
 
